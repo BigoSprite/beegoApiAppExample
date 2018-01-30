2366c3a01a74a8e6b3a8574bad215cc57968cf26
bigosprite <bigosprite>
2018-01-30 14:37:19 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	_ "github.com/BigoSprite/beegoApiAppExample/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+	orm.RegisterDataBase("default", "mysql", "root:root@tcp(127.0.0.1:3306)/api")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
+
---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,50 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["github.com/BigoSprite/beegoApiAppExample/controllers:StudentController"] = append(beego.GlobalControllerRouter["github.com/BigoSprite/beegoApiAppExample/controllers:StudentController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/BigoSprite/beegoApiAppExample/controllers:StudentController"] = append(beego.GlobalControllerRouter["github.com/BigoSprite/beegoApiAppExample/controllers:StudentController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/BigoSprite/beegoApiAppExample/controllers:StudentController"] = append(beego.GlobalControllerRouter["github.com/BigoSprite/beegoApiAppExample/controllers:StudentController"],
+		beego.ControllerComments{
+			Method: "GetOne",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/BigoSprite/beegoApiAppExample/controllers:StudentController"] = append(beego.GlobalControllerRouter["github.com/BigoSprite/beegoApiAppExample/controllers:StudentController"],
+		beego.ControllerComments{
+			Method: "Put",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/BigoSprite/beegoApiAppExample/controllers:StudentController"] = append(beego.GlobalControllerRouter["github.com/BigoSprite/beegoApiAppExample/controllers:StudentController"],
+		beego.ControllerComments{
+			Method: "Delete",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+}
